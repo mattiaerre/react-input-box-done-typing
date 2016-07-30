7d1fa536ffd820e5992580e7e5ca62579a8e250f
mattia richetto <mrichetto@opentable.com>
2016-07-30 22:29:27 +0100

    add demo

---
+++ demo/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InputBoxDoneTyping from '../src/InputBoxDoneTyping';
+
+const App = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <InputBoxDoneTyping
+          inputClassName="form-control"
+          inputOnChange={(value) => { console.log('inputOnChange:', value); } }
+          inputDefaultValue="lon"
+          doneTypingInterval={2000}
+          inputDoneTyping={(value) => { console.log('inputDoneTyping:', value); } }
+          />
+      </div>
+      <div className="row">
+        <InputBoxDoneTyping
+          inputClassName="form-control"
+          inputDoneTyping={(value) => { console.log('inputDoneTyping:', value); } }
+          />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
